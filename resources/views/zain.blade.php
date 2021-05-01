01622dcf8d501487d7d200722c52de755d81562e
Cong Fandi <cong.fandi@abersoft.se>
2021-05-01 07:21:35 +0700

    adding page login , profject, sidebar,navbar and also making blade template with section

---
+++ resources/views/zain.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('content')
+<h1>ZAINULLAH</h1>
+<h2>Fandi</h2>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/tambah', function () {
+    return view('tambah');
+});
+
+
+Route::get('/zain', function () {
+    return view('zain');
+});
+
+
+Route::get('/projects', function () {
+    return view('projects');
+});
